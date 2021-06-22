e0be7b224695cc2686bdb93706b80e5b52a6d0b4
jcooperation0137 <jcooperation0137@gmail.com>
2021-06-22 05:28:10 -0400

    dev-3905-android: change android qr code plugin

--- android/src/main/java/asia/diningcity/qrscan/QrScanner.java
+++ android/src/main/java/asia/diningcity/qrscan/QrScanner.java
@@ -1,20 +1,48 @@
 package asia.diningcity.qrscan;
 
+import android.content.Intent;
+
 import com.getcapacitor.JSObject;
 import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 
-@NativePlugin
+@NativePlugin(
+        requestCodes = {QrScanner.REQUEST_CODE_SCAN}
+)
 public class QrScanner extends Plugin {
+    protected static final int REQUEST_CODE_SCAN = 2;
 
     @PluginMethod
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
+    public void scanQrCode(PluginCall call) {
+        saveCall(call);
 
-        JSObject ret = new JSObject();
-        ret.put("value", value);
-        call.success(ret);
+        Intent scanIntent = new Intent(getContext(), CodeScannerActivity.class);
+        startActivityForResult(call, scanIntent, REQUEST_CODE_SCAN);
+    }
+
+    @Override
+    protected void handleOnActivityResult(int requestCode, int resultCode, Intent data) {
+        super.handleOnActivityResult(requestCode, resultCode, data);
+
+        PluginCall savedCall = getSavedCall();
+        if (savedCall == null) return;
+
+        if (requestCode == REQUEST_CODE_SCAN) {
+            if (resultCode == 0) {
+                String qrCode = data.getStringExtra("RESULT_QR_CODE");
+                JSObject ret = new JSObject();
+                ret.put("result", qrCode);
+                savedCall.success(ret);
+            } else {
+                String error = data.getStringExtra("RESULT_ERROR");
+                if (error != null) {
+                    savedCall.reject(error);
+                    return;
+                }
+                savedCall.reject("Error occurred when get a QR code.");
+            }
+        }
     }
 }
