50dffdaa87a41b4734167f3fabdbb3f01b19ec66
EddyFisher <jinjinlee890428@hotmail.com>
2021-07-21 15:43:12 +0800

    Changed plugin

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,19 +1,4 @@
-// declare module '@capacitor/core' {
-//   interface PluginRegistry {
-//     QrScanner: QrScannerPlugin;
-//   }
-// }
-
-import type { PermissionState } from '@capacitor/core';
-
 
 export interface QrScannerPlugin {
   echo(options: { value: string }): Promise<{ value: string }>;
-  checkPermissions(): Promise<PermissionStatus>;
-  requestPermissions(): Promise<PermissionStatus>;
-  scanQrCode(): Promise<{ value: string }>;
-}
-
-export interface PermissionStatus {
-  camera: PermissionState;
 }
--- src/web.ts
+++ src/web.ts
@@ -1,29 +1,10 @@
 import { WebPlugin } from '@capacitor/core';
-import { QrScannerPlugin, PermissionStatus } from './definitions';
+import { QrScannerPlugin } from './definitions';
 
 export class QrScannerWeb extends WebPlugin implements QrScannerPlugin {
-  constructor() {
-    super({
-      name: 'QrScanner',
-      platforms: ['web'],
-    });
-  }
-
   async echo(options: { value: string }): Promise<{ value: string }> {
     console.log('ECHO', options);
     return options;
-  }
-
-  async checkPermissions(): Promise<PermissionStatus> {
-    throw this.unavailable('Permissions API not available in this browser.');
-  }
-
-  async requestPermissions(): Promise<PermissionStatus> {
-    throw this.unavailable('Permissions API not available in this browser.');
-  }
-
-  async scanQrCode(): Promise<{ value: string }> {
-    throw this.unavailable('SacnQRCode not available in this browser.');
   }
 }
 
